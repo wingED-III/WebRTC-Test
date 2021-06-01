1571cd597b6a1e70bf0b7c165b33f1be301bfd53
wingED-III <60010106@kmitl.ac.th>
2021-06-01 22:44:39 +0700

    copy paste css

--- webrtc_test/src/App.js
+++ webrtc_test/src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import TextField from "@material-ui/core/TextField"
+import AssignmentIcon from "@material-ui/icons/Assignment"
+import PhoneIcon from "@material-ui/icons/Phone"
+import React, { useEffect, useRef, useState } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import Peer from "simple-peer"
+import io from "socket.io-client"
+import "./App.css"
 
 function App() {
   return (
